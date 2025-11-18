37ae4fb817b17b442cfdb1bff52528e5aff8f4bf
Szabó Barnabás <szabo.barnabas120@gmail.com>
2025-11-18 14:22:48 +0100

    jaaaaaaaa

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -45,6 +45,44 @@ class AlbumController extends Controller
         ]);
     }
 
+    /**
+     * @api {get} /album/:id/songs Get songs for an album
+     * @apiName GetAlbumSongs
+     * @apiGroup Album
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} id Album ID.
+     *
+     * @apiSuccess {String} album Album name.
+     * @apiSuccess {Object[]} songs List of songs for the album.
+     * @apiSuccess {Number} songs.id Song ID.
+     * @apiSuccess {String} songs.name Song name.
+     * @apiSuccess {String} songs.lyrics Song lyrics.
+     * @apiSuccess {String} songs.songwriter Songwriter name.
+     * @apiSuccess {Number} songs.album_id Associated album ID.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "album": "The Life of a Showgirl",
+     *       "songs": [
+     *         {
+     *           "id": 1,
+     *           "name": "Nikes",
+     *           "lyrics": "",
+     *           "songwriter": "Christopher Breaux",
+     *           "album_id": 27
+     *         }
+     *       ]
+     *     }
+     *
+     * @apiError (404) AlbumNotFound The specified album was not found.
+     * @apiErrorExample {json} Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "message": "Album not found"
+     *     }
+     */
     public function index_song($id) {
         $album = Album::find($id);
 
@@ -97,6 +135,45 @@ class AlbumController extends Controller
 
         return response()->json(['message' => 'Album created successfully', 'album' => $album], 201);
     }
+    /**
+     * @api {post} /album/:id/song Create a new song for an album
+     * @apiName CreateAlbumSong
+     * @apiGroup Album
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} id Album ID.
+     *
+     * @apiBody {String} name Song name.
+     * @apiBody {String} [lyrics] Song lyrics.
+     * @apiBody {String} [songwriter] Songwriter name.
+     *
+     * @apiSuccess {Object} song Created song object.
+     * @apiSuccess {Number} song.id Song ID.
+     * @apiSuccess {String} song.name Song name.
+     * @apiSuccess {String} song.lyrics Song lyrics.
+     * @apiSuccess {String} song.songwriter Songwriter name.
+     * @apiSuccess {Number} song.album_id Associated album ID.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 201 Created
+     *     {
+     *       "message": "Song created successfully",
+     *       "song": {
+     *         "id": 12,
+     *         "name": "Imagine",
+     *         "lyrics": "Imagine there's no heaven...",
+     *         "songwriter": "John Lennon",
+     *         "album_id": 2
+     *       }
+     *     }
+     *
+     * @apiError (404) AlbumNotFound The specified album was not found.
+     * @apiErrorExample {json} Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "message": "Album not found"
+     *     }
+     */
     public function store_song(Request $request, $id)
     {
         $album = Album::find($id);
@@ -155,6 +232,48 @@ class AlbumController extends Controller
 			'album' => $album,
 		]);
 	} 
+    /**
+     * @api {patch} /album/:album_id/song/:id Update a song for an album
+     * @apiName UpdateAlbumSong
+     * @apiGroup Album
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} album_id Album ID.
+     * @apiParam {Number} id Song ID.
+     *
+     * @apiBody {String} [name] Song name.
+     * @apiBody {String} [lyrics] Song lyrics.
+     * @apiBody {String} [songwriter] Songwriter name.
+     *
+     * @apiSuccess {String} message Confirmation message.
+     * @apiSuccess {Object} song Updated song object.
+     * @apiSuccess {Number} song.id Song ID.
+     * @apiSuccess {String} song.name Song name.
+     * @apiSuccess {String} song.lyrics Song lyrics.
+     * @apiSuccess {String} song.songwriter Songwriter name.
+     * @apiSuccess {Number} song.album_id Associated album ID.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "message": "Song updated successfully",
+     *       "song": {
+     *         "id": 3,
+     *         "name": "Updated Song",
+     *         "lyrics": "Updated lyrics...",
+     *         "songwriter": "Updated Writer",
+     *         "album_id": 2
+     *       }
+     *     }
+     *
+     * @apiError (404) AlbumNotFound The specified album was not found.
+     * @apiError (404) SongNotFound The specified song was not found for this album.
+     * @apiErrorExample {json} Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "message": "Song not found for this album"
+     *     }
+     */
     public function update_song(SongRequest $request, $album_id, $id)
     {
 
@@ -204,6 +323,33 @@ class AlbumController extends Controller
     ], 410);
     }
 
+    /**
+     * @api {delete} /album/:album_id/song/:id Delete a song from an album
+     * @apiName DeleteAlbumSong
+     * @apiGroup Album
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} album_id Album ID.
+     * @apiParam {Number} id Song ID.
+     *
+     * @apiSuccess {String} message Confirmation message.
+     * @apiSuccess {Number} id Deleted song ID.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "message": "Song deleted successfully",
+     *       "id": 5
+     *     }
+     *
+     * @apiError (404) AlbumNotFound The specified album was not found.
+     * @apiError (404) SongNotFound The specified song was not found for this album.
+     * @apiErrorExample {json} Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "message": "Song not found for this album"
+     *     }
+     */
     public function destroy_song($album_id, $id)
     {
         $album = Album::find($album_id);
--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -66,6 +66,46 @@ class ArtistController extends Controller
         ]);
     }
 
+    /**
+     * @api {get} /artist/:id/albums Get albums for an artist
+     * @apiName GetArtistAlbums
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} id Artist ID.
+     *
+     * @apiSuccess {String} artist Artist name.
+     * @apiSuccess {Object[]} albums List of albums for the artist.
+     * @apiSuccess {Number} albums.id Album ID.
+     * @apiSuccess {String} albums.name Album name.
+     * @apiSuccess {String} albums.cover Album cover URL.
+     * @apiSuccess {String} albums.genre Album genre.
+     * @apiSuccess {Number} albums.artist_id Associated artist ID.
+     * @apiSuccess {String} albums.year Release year.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "artist": "Taylor Swift",
+     *       "albums": [
+     *         {
+     *           "id": 1,
+     *           "name": "1989",
+     *           "cover": "https://...",
+     *           "genre": "Pop",
+     *           "artist_id": 1,
+     *           "year": "2014"
+     *         }
+     *       ]
+     *     }
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     * @apiErrorExample {json} Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "message": "Artist not found"
+     *     }
+     */
     public function index_album($id)
     {
         $artist = Artist::find($id);
@@ -80,6 +120,32 @@ class ArtistController extends Controller
         ]);
     }
 
+    /**
+     * @api {get} /artist/:artist_id/album/:id/songs Get songs for an artist's album
+     * @apiName GetArtistAlbumSongs
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} artist_id Artist ID.
+     * @apiParam {Number} id Album ID.
+     *
+     * @apiSuccess {String} artist Artist name.
+     * @apiSuccess {String} album Album name.
+     * @apiSuccess {Object[]} songs List of songs for the album.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "artist": "Artist Name",
+     *       "album": "Album Name",
+     *       "songs": [
+     *         { "id": 1, "name": "Track 1", "album_id": 2 }
+     *       ]
+     *     }
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     * @apiError (404) AlbumNotFound The specified album was not found for this artist.
+     */
     public function index_song($artist_id,$id)
     {
         $artist = Artist::find($artist_id);
@@ -143,6 +209,24 @@ class ArtistController extends Controller
 
         return response(json_encode(['artist' => $artist,]),201);
     }
+    /**
+     * @api {post} /artist/:id/member Create a new member for a band
+     * @apiName CreateArtistMember
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} id Artist ID.
+     *
+     * @apiBody {String} name Member name.
+     * @apiBody {String} instrument Member instrument.
+     * @apiBody {String} year Year joined or active.
+     * @apiBody {String} image Member image URL.
+     *
+     * @apiSuccess {Object} member Created member object.
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     * @apiError (400) NotABand The artist is not a band.
+     */
     public function store_member(MemberRequest $request, $id)
     {
         $artist = Artist::find($id);
@@ -160,6 +244,23 @@ class ArtistController extends Controller
         return response()->json(['message' => 'Member created successfully', 'member' => $member], 201);
     }
 
+    /**
+     * @api {post} /artist/:id/album Create a new album for an artist
+     * @apiName CreateArtistAlbum
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} id Artist ID.
+     *
+     * @apiBody {String} name Album name.
+     * @apiBody {String} cover Album cover URL.
+     * @apiBody {String} genre Album genre.
+     * @apiBody {String} year Release year.
+     *
+     * @apiSuccess {Object} album Created album object.
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     */
     public function store_album(AlbumRequest $request, $id)
     {
         $artist = Artist::find($id);
@@ -173,6 +274,24 @@ class ArtistController extends Controller
         return response()->json(['message' => 'Album created successfully', 'album' => $album], 201);
     }
 
+    /**
+     * @api {post} /artist/:artist_id/album/:id/song Create a new song for an album
+     * @apiName CreateArtistAlbumSong
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} artist_id Artist ID.
+     * @apiParam {Number} id Album ID.
+     *
+     * @apiBody {String} name Song name.
+     * @apiBody {String} [lyrics] Song lyrics.
+     * @apiBody {String} [songwriter] Songwriter name.
+     *
+     * @apiSuccess {Object} song Created song object.
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     * @apiError (404) AlbumNotFound The specified album was not found for this artist.
+     */
     public function store_song(SongRequest $request, $artist_id, $id)
     {
         $artist = Artist::find($artist_id);
@@ -239,7 +358,26 @@ class ArtistController extends Controller
 			'artist' => $artist,
 		]);
         
-	}public function update_member(MemberRequest $request, $artist_id, $id)
+    }
+    /**
+     * @api {patch} /artist/:artist_id/member/:id Update a member for an artist
+     * @apiName UpdateArtistMember
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} artist_id Artist ID.
+     * @apiParam {Number} id Member ID.
+     *
+     * @apiBody {String} [name] Member name.
+     * @apiBody {String} [instrument] Member instrument.
+     * @apiBody {String} [year] Year joined or active.
+     *
+     * @apiSuccess {Object} member Updated member object.
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     * @apiError (404) MemberNotFound The specified member was not found for this artist.
+     */
+    public function update_member(MemberRequest $request, $artist_id, $id)
     {
 
         $artist = Artist::find($artist_id);
@@ -263,6 +401,25 @@ class ArtistController extends Controller
         return response()->json(['message' => 'Member updated successfully', 'member' => $member]);
     }
 
+    /**
+     * @api {patch} /artist/:artist_id/album/:id Update an album for an artist
+     * @apiName UpdateArtistAlbum
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} artist_id Artist ID.
+     * @apiParam {Number} id Album ID.
+     *
+     * @apiBody {String} [name] Album name.
+     * @apiBody {String} [cover] Album cover URL.
+     * @apiBody {String} [genre] Album genre.
+     * @apiBody {String} [year] Release year.
+     *
+     * @apiSuccess {Object} album Updated album object.
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     * @apiError (404) AlbumNotFound The specified album was not found for this artist.
+     */
     public function update_album(AlbumRequest $request, $artist_id, $id)
     {
 
@@ -283,6 +440,26 @@ class ArtistController extends Controller
         return response()->json(['message' => 'Album updated successfully', 'album' => $album]);
     }
 
+    /**
+     * @api {patch} /artist/:artist_id/album/:album_id/song/:id Update a song for an artist's album
+     * @apiName UpdateArtistAlbumSong
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} artist_id Artist ID.
+     * @apiParam {Number} album_id Album ID.
+     * @apiParam {Number} id Song ID.
+     *
+     * @apiBody {String} [name] Song name.
+     * @apiBody {String} [lyrics] Song lyrics.
+     * @apiBody {String} [songwriter] Songwriter name.
+     *
+     * @apiSuccess {Object} song Updated song object.
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     * @apiError (404) AlbumNotFound The specified album was not found for this artist.
+     * @apiError (404) SongNotFound The specified song was not found for this album.
+     */
     public function update_song(SongRequest $request, $artist_id, $album_id, $id)
     {
 
@@ -338,7 +515,22 @@ class ArtistController extends Controller
     ], 410);
 
     }
-     public function destroy_member($artist_id, $id)
+    /**
+    * @api {delete} /artist/:artist_id/member/:id Delete a member from an artist
+    * @apiName DeleteArtistMember
+    * @apiGroup Artist
+    * @apiVersion 1.0.0
+    *
+    * @apiParam {Number} artist_id Artist ID.
+    * @apiParam {Number} id Member ID.
+    *
+    * @apiSuccess {String} message Confirmation message.
+    * @apiSuccess {Number} id Deleted member ID.
+    *
+    * @apiError (404) ArtistNotFound The specified artist was not found.
+    * @apiError (404) MemberNotFound The specified member was not found for this artist.
+    */
+    public function destroy_member($artist_id, $id)
     {
         $artist = Artist::find($artist_id);
 
@@ -361,6 +553,21 @@ class ArtistController extends Controller
         return response()->json(['message' => 'Member deleted successfully', 'id' => $id], 410);
     }
 
+    /**
+     * @api {delete} /artist/:artist_id/album/:id Delete an album from an artist
+     * @apiName DeleteArtistAlbum
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} artist_id Artist ID.
+     * @apiParam {Number} id Album ID.
+     *
+     * @apiSuccess {String} message Confirmation message.
+     * @apiSuccess {Number} id Deleted album ID.
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     * @apiError (404) AlbumNotFound The specified album was not found for this artist.
+     */
     public function destroy_album($artist_id, $id)
     {
         $artist = Artist::find($artist_id);
@@ -381,6 +588,23 @@ class ArtistController extends Controller
         return response()->json(['message' => 'Album deleted successfully', 'id' => $id], 410);
     }
 
+    /**
+     * @api {delete} /artist/:artist_id/album/:album_id/song/:id Delete a song from an artist's album
+     * @apiName DeleteArtistAlbumSong
+     * @apiGroup Artist
+     * @apiVersion 1.0.0
+     *
+     * @apiParam {Number} artist_id Artist ID.
+     * @apiParam {Number} album_id Album ID.
+     * @apiParam {Number} id Song ID.
+     *
+     * @apiSuccess {String} message Confirmation message.
+     * @apiSuccess {Number} id Deleted song ID.
+     *
+     * @apiError (404) ArtistNotFound The specified artist was not found.
+     * @apiError (404) AlbumNotFound The specified album was not found for this artist.
+     * @apiError (404) SongNotFound The specified song was not found for this album.
+     */
     public function destroy_song($artist_id, $album_id, $id)
     {
         $artist = Artist::find($artist_id);
